84950c42450ee5edb7e9bc0c05e9dc46ee60fd6c
khaledze <k.aliahmed@ecole-ipssi.net>
2023-11-16 21:27:50 +0100

    modification du bouton louer

--- location/src/components/Jeu.jsx
+++ location/src/components/Jeu.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import './card.css';
+import React, { useEffect, useState } from "react";
+import "./card.css";
 
 export default function Jeu() {
   const [jeux, setJeux] = useState([]);
   const [selectedJeu, setSelectedJeu] = useState(null);
   const [showRentPage, setShowRentPage] = useState(false);
-  const [rentalDate, setRentalDate] = useState('');
-  const [returnDate, setReturnDate] = useState('');
+  const [rentalDate, setRentalDate] = useState("");
+  const [returnDate, setReturnDate] = useState("");
 
   useEffect(() => {
     const fetchJeux = async () => {
       try {
-        const response = await fetch('http://localhost:3001/jeux');
+        const response = await fetch("http://localhost:3001/jeux");
         const data = await response.json();
         setJeux(data);
       } catch (error) {
@@ -29,54 +29,68 @@ export default function Jeu() {
 
   const renderRentPage = () => {
     const calculatePrice = () => {
-      
-      const pricePerDay = selectedJeu?.prix || 0; 
+      const pricePerDay = selectedJeu?.prix || 0;
       const startDate = new Date(rentalDate);
       const endDate = new Date(returnDate);
-      const numberOfDays = (endDate - startDate) / (1000 * 60 * 60 * 24); 
-  
+      const numberOfDays = (endDate - startDate) / (1000 * 60 * 60 * 24);
+
       return pricePerDay * numberOfDays;
     };
-  
+
     const totalPrice = calculatePrice();
-  
+
     const handleLouerClick = () => {
       const totalPrice = calculatePrice();
-      console.log(`Game rented from ${rentalDate} to ${returnDate}. Total price: ${totalPrice}€`);
-     
+      console.log(
+        `Game rented from ${rentalDate} to ${returnDate}. Total price: ${totalPrice}€`
+      );
+
       setShowRentPage(false);
       setSelectedJeu(null);
-      setRentalDate('');
-      setReturnDate('');
+      setRentalDate("");
+      setReturnDate("");
     };
-  
+
     return (
       <div className="rent-page">
-        <button className="close-button" onClick={() => setShowRentPage(false)}>X</button>
+        <button className="close-button" onClick={() => setShowRentPage(false)}>
+          X
+        </button>
         <h2>{selectedJeu?.nom_jeu}</h2>
         <p>Price: {selectedJeu?.prix}€</p>
         <label>Rental date:</label>
-        <input type="date" value={rentalDate} onChange={(e) => setRentalDate(e.target.value)} />
+        <input
+          type="date"
+          value={rentalDate}
+          onChange={(e) => setRentalDate(e.target.value)}
+        />
         <label>Return date:</label>
-        <input type="date" value={returnDate} onChange={(e) => setReturnDate(e.target.value)} />
+        <input
+          type="date"
+          value={returnDate}
+          onChange={(e) => setReturnDate(e.target.value)}
+        />
         <p>Total price: {totalPrice}€</p>
         <button onClick={handleLouerClick}>Rent</button>
       </div>
     );
   };
-  
 
   return (
     <div className="jeux-container">
-      {jeux.map(jeu => (
+      {jeux.map((jeu) => (
         <div key={jeu.id} className="card">
           <h2>{jeu.nom_jeu}</h2>
           <p>Prix: {jeu.prix}€</p>
-          <button onClick={() => handleObtenirClick(jeu)}>Obtenir</button>
+          <img
+            src={require("../img/add.svg").default}
+            alt="Obtenir"
+            style={{ width: "50px", height: "30px", cursor: "pointer" }}
+            onClick={() => handleObtenirClick(jeu)}
+          />
         </div>
       ))}
       {showRentPage && renderRentPage()}
     </div>
   );
 }
-
